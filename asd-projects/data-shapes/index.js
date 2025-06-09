377b5bfe222c6d42461445dd7346d15a867104da
vincenth411 <stu.vincent.hall@ihsnola.org>
2025-06-09 19:08:19 +0000

    Done with data shapes

--- asd-projects/data-shapes/index.js
+++ asd-projects/data-shapes/index.js
@@ -63,27 +63,35 @@ $(document).ready(function () {
   function handleGood(color, shape, repeat) {
     setBackgroundWithSimple(color, shape, repeat);
     animationDetails.displayType = 2;
-
   }
   // TODO 5-a: add a function that handles the bad display type
-
+  function handleBad(data, repeat) {
+    repeat++;
+    setBackgroundWithMixed(data, repeat);
+    animationDetails.displayType = 3;
+  }
   /////////////////////////////////////////////////
   // BUTTON HANDLERS BELOW HERE (3-b, 4-b, 5-b) ///
   /////////////////////////////////////////////////
 
   function staticDisplay() {
     // TODO 3-b: call your handleStatic function
-    handleStatic(dataShapes[currentIndex])
+    handleStatic(dataShapes[currentIndex]);
   }
 
   function goodDisplay() {
+    var currentShape = dataShapes[currentIndex];
+    handleGood(currentShape.color, currentShape.shape, currentShape.repeat);
+
     // TODO 4-b: call your handleGood function
-    var currentShape
   }
 
   function badDisplay() {
-    // TODO 5-b: call your handleBad function
+    var currentShape = dataShapes[currentIndex];
+    var repeat = currentShape.repeat;
+    handleBad(currentShape, repeat);
 
+    // TODO 5-b: call your handleBad function
   }
 
   /////////////////////////////////////////////////
